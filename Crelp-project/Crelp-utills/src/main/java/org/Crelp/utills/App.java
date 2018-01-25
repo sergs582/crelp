1fc7bbd91a4e1218c16a3ade1cf8dd2566f52c7e
sergey <sergey@192.168.31.200>
2018-01-25 21:32:56 +0300

    adding utills module

---
+++ Crelp-project/Crelp-utills/src/main/java/org/Crelp/utills/App.java
@@ -0,0 +1,13 @@
+package org.Crelp.utills;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
