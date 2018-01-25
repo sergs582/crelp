3e0bb7f85b9cea05bb38dc49397d62b28b2d0ee6
sergey <sergey@192.168.31.200>
2018-01-25 21:30:47 +0300

    Uploading project to GIT

---
+++ Crelp-project/src/main/java/by/idealabs/crelp/crelp_project/App.java
@@ -0,0 +1,13 @@
+package by.idealabs.crelp.crelp_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
